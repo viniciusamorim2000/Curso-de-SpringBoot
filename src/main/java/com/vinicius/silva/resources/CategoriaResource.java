95bb034b48deef18f5ea8e47dbc83e9bfcfadf77
Vinicius <amorimhavoc@gmail.com>
2020-03-10 14:10:35 -0300

    Banco de dados H2 e criação automatica das tabelas

--- src/main/java/com/vinicius/silva/resources/CategoriaResource.java
+++ src/main/java/com/vinicius/silva/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.vinicius.silva.resources;
 
+import com.vinicius.silva.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public String listar () {
-		return "Rest está OK";
-	}
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List <Categoria> listar (){
+
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2,"Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+    }
 }
