09f7e352c4dec0b686bc6ee9be99459724a8b600
Vinicius <amorimhavoc@gmail.com>
2020-03-10 12:20:41 -0300

    Projeto criado

---
+++ src/main/java/com/vinicius/silva/CursospmcApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.silva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinicius/silva/CursospmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.silva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
